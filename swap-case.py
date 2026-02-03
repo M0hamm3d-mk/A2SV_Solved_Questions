1d143f0271a6e2e4d22aedf983781c8ed7318ce9
Mohammed Moges <mohammedmoges29@gmail.com>
2026-02-03 12:15:34 +0300

    Add files via upload

---
+++ swap-case.py
@@ -0,0 +1,8 @@
+def swap_case(s):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].islower():
+            s[i] = s[i].upper()
+        elif s[i].isupper():
+            s[i] = s[i].lower()
+    return "".join(s)
