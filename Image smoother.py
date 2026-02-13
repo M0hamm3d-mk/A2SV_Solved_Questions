5adba6280cab99893298579a3f53c696b06dc44a
Mohammed Moges <mohammedmoges29@gmail.com>
2026-02-13 15:23:38 +0300

    Image smoother.py

--- Image smoother.py
+++ Image smoother.py
@@ -34,3 +34,25 @@ class Solution:
                 smother[r][c] = total//valid
         return smother
 
+
+# optional and clean way
+
+# class Solution:
+#     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+#         row,col = len(img),len(img[0])
+#         smother = [[0 for _ in range(col)] for _ in range(row)]
+#         for r in range(row):
+#             for c in range(col):
+#                 valid = 0
+#                 total = 0
+#                 for i in range(-1,2):
+#                     for j in range(-1,2):
+#                         curr_r = r + i
+#                         curr_c = c + j
+#                         if 0 <= curr_r < row and 0 <= curr_c < col:
+#                             total += img[curr_r][curr_c]
+#                             valid += 1
+#                 smother[r][c] = total//valid
+#         return smother
+
+
