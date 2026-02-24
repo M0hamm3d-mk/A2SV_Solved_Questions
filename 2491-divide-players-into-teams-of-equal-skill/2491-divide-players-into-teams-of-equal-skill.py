59574f6ac796ff05237cfb41eb754f076f8cdff8
Mohammed Moges <mohammedmoges29@gmail.com>
2026-02-24 16:05:23 +0300

    Update 2491-divide-players-into-teams-of-equal-skill.py

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,24 +1,18 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        if sum(skill) % (len(skill) / 2) != 0:
-            return -1
-        target = sum(skill) / (len(skill) / 2)
         skill.sort()
-        left = 0
-        sums = 0
-        right = len(skill) - 1
-        single = 0
-        while left < right:
-            if skill[left] + skill[right] == target:
-                sums += (skill[left] * skill[right])
-                left += 1 
-                right -= 1
-            elif skill[left] + skill[right] > target:
-                right -= 1
-                single += 1
-            elif skill[left] + skill[right] < target:
-                single += 1
-                left += 1
-        if single > 0:
-            return -1
-        return sums
+        mid = len(skill)//2
+        left,right = 0,len(skill)-1
+        chemistry = 0
+        total_chemistry = 0
+
+        while left < mid and right >= mid:
+            if skill[left] + skill[right] != chemistry and chemistry != 0:
+                return -1
+            chemistry = skill[left] + skill[right]
+            total_chemistry += skill[left] * skill[right]
+            left += 1
+            right -= 1
+            
+        return total_chemistry
+
