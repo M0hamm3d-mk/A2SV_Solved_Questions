df565ebc9ee159f11adcb02d68ff95ce9f6317bd
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-09 22:51:34 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+    
+        for i in nums1:
+            temp = nums2.copy()
+            nearest_large = i
+
+            while temp[-1] != i:
+                if temp[-1] > i:
+                    nearest_large = temp[-1]
+                temp.pop()
+
+            if nearest_large == i:
+                res.append(-1)
+            else:
+                res.append(nearest_large)
+                
+        return res
+            
