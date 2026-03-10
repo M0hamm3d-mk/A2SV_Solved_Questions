a99a44fff6b886e85d2f7574ff8a56e7e0b3aaf1
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-10 09:04:16 +0300

    Update 0496-Next-Greater-Element-I.py

--- leetcode/0496-Next-Greater-Element-I/0496-Next-Greater-Element-I.py
+++ leetcode/0496-Next-Greater-Element-I/0496-Next-Greater-Element-I.py
@@ -17,3 +17,21 @@ class Solution:
                 res.append(nearest_large)
                 
         return res
+
+
+# optimal solution
+# class Solution:
+#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         res = []
+#         stack = []
+#         next_g = {}
+#         for n in nums2:
+#             while stack and stack[-1] < n:
+#                 next_g[stack.pop()] = n
+#             stack.append(n)
+#         for n in nums1:
+#             if n not in next_g:
+#                 res.append(-1) 
+#             else:
+#                 res.append(next_g[n])
+#         return res
