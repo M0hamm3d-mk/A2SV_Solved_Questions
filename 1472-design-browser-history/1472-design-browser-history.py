9b02e5466995991b3e8de8083b10501628fba6d5
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-11 10:08:12 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -19,28 +19,19 @@ class BrowserHistory:
 
     def back(self, steps: int) -> str:
     
-        while self.curr and steps:
+        while self.curr.prev and steps:
             self.curr = self.curr.prev
             steps -= 1
 
-        if not self.curr:
-            # self.tail = self.home
-            self.curr = self.home
-            return self.home.val
-        else:
-            return self.curr.val
+        return self.curr.val
 
     def forward(self, steps: int) -> str:
 
-        while self.curr and steps:
+        while self.curr.next and steps:
             self.curr = self.curr.next
             steps -= 1
 
-        if not self.curr:
-            self.curr = self.tail
-            return self.tail.val
-        else:
-            return self.curr.val
+        return self.curr.val
 
 
 
