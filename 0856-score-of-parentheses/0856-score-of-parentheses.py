a1a83342eab937a054e57e868eb9489be0af292d
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-13 15:11:59 +0300

    Update 0856-score-of-parentheses.py

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -13,4 +13,24 @@ class Solution:
                     scores[-1] += popped * 2
             stack.append(p)
         return scores[0]
+
+# optimization
+# we can do it with O(1) space 
+# class Solution:
+#     def scoreOfParentheses(self, s: str) -> int:
+#         n = len(s)
+#         score = 0
+#         depth = 0 
+
+#         for i in range(n):
+#             if s[i] == '(':
+#                 depth += 1
+#             else:
+#                 if s[i-1] == '(':
+#                     depth -= 1
+#                     score += 2 ** depth
+#                 else:
+#                     depth -= 1
+                    
+#         return score
         
