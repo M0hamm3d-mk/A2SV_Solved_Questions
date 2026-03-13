53732e09571e8554b55e5109b5316b3f47ed74e1
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-13 15:22:48 +0300

    Add solution for Design Circular Deque

--- leetcode/0641-Design-Circular-Deque/0641-Design-Circular-Deque.py
+++ leetcode/0641-Design-Circular-Deque/0641-Design-Circular-Deque.py
@@ -1,50 +1,81 @@
+class ListNode():
+    def __init__(self,val=-1,next=None,prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
 class MyCircularDeque:
 
     def __init__(self, k: int):
-        self.dq = deque()
         self.k = k
+        self.front = ListNode()
+        self.rear = self.front
+        self.n = 0
 
     def insertFront(self, value: int) -> bool:
-        if len(self.dq) == self.k:
+        if self.n >= self.k:
             return False
-        self.dq.appendleft(value)
+        if self.n == 0 or self.front.val == -1:
+            node = ListNode(value)
+            self.front = node
+            self.rear = node
+        else:
+            node = ListNode(value,self.front,)
+            self.front.prev = node
+            self.front = node
+        self.n += 1
         return True
-
 
     def insertLast(self, value: int) -> bool:
-        if len(self.dq) == self.k:
+        if self.n >= self.k:
             return False
-        self.dq.append(value)
+        
+        if self.n == 0 or self.rear.val == -1:
+            node = ListNode(value)
+            self.rear = node
+            self.front = node
+        else:
+            node = ListNode(value,None,self.rear)
+            self.rear.next = node
+            self.rear = node
+        self.n += 1
         return True
+        
 
     def deleteFront(self) -> bool:
-        if self.dq:
-            self.dq.popleft()
-            return True
-        return False
+        if self.n == 0 or  self.front.val == -1:
+            return False
+        self.front = self.front.next
+        if self.front:
+            self.front.prev = None 
+        self.n -= 1
+        return True
 
     def deleteLast(self) -> bool:
-        if self.dq:
-            self.dq.pop()
-            return True
-        return False
+        if self.n == 0 or self.rear.val == -1 :
+            return False
+        self.rear = self.rear.prev
+        self.n -= 1
+        return True
         
 
     def getFront(self) -> int:
-        if self.dq:
-            return self.dq[0]
-        return -1
+        if self.n == 0:
+            return -1
+        return self.front.val
+        
 
     def getRear(self) -> int:
-        if self.dq:
-            return self.dq[-1]
-        return -1
+        if self.n == 0:
+            return -1
+        return self.rear.val
         
+
     def isEmpty(self) -> bool:
-        return not self.dq
+        return self.n == 0
+        
 
     def isFull(self) -> bool:
-        return len(self.dq) == self.k
+        return self.n == self.k
         
 
 
