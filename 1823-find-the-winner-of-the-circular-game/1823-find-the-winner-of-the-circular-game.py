3d0592281b5b38da451c28322222a826f923b1b4
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-22 21:56:32 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -8,18 +8,8 @@ class Solution:
             size = m * k + 2
             for i in range(size):
                 if i % size == k - 1:
-                    # if the last player is gonna be elliminated
-                    if (k-1) % m == m - 1:
-                       players.pop()
-                       return helper(players,k)
-
-                    # if the first player is gonna be elliminated
-                    elif (k-1) % m == 0:
-                       return helper(players[1:],k)
-
-                    # any player in the middle is gonna be elliminated
-                    else:
-                        return helper(players[k % m:] + players[:(k-1)%m],k)
+                    players = players[(k - 1)%m + 1:] + players[:(k-1)%m]
+                    return helper(players,k)
                         
         return helper(nums,k)
         
