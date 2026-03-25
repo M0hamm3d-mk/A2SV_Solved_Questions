c09da7101eaf594eff0bfd8dd6f160333636e42f
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-25 18:32:43 +0300

    Time: 108 ms (55.26%), Space: 61.2 MB (90.76%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def helper(first,path):
+            if len(path) == k:
+                ans.append(path[:])
+                return 
+
+            for i in range(first,n+1):
+                path.append(i)
+                helper(i+1,path)
+                path.pop()
+        ans = []
+        helper(1,[])
+        return ans
+       
+        
