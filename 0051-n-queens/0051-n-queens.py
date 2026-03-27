3f5dc7e8f8fb32845e2bb97d0ad465180c153ff8
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-27 04:21:06 +0300

    Time: 11 ms (68.5%), Space: 19.7 MB (54.05%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board = [['.'] * n  for _ in range(n)]
+        colSet = set()
+        decDiag = set()
+        incDiag = set()
+        def backTrack(row):
+            if row == n :
+                res.append(["".join(r) for r in board[:]])
+                return
+            for col in range(n):
+                if col not in colSet and  row - col not in decDiag and row + col not in incDiag:
+                    colSet.add(col)
+                    decDiag.add(row-col)
+                    incDiag.add(row+col)
+                    board[row][col] = 'Q'
+
+                    backTrack(row+1)
+
+                    colSet.remove(col)
+                    decDiag.remove(row-col)
+                    incDiag.remove(row+col)
+                    board[row][col] = '.'
+        backTrack(0)
+
+        return res
+
