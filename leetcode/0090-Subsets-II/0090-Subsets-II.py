2c92037b6fc3470bb41cb6d5b152ea47071002e9
Mohammed Moges <mohammedmoges29@gmail.com>
2026-03-29 03:08:07 +0300

    Add solution for Subsets II

---
+++ leetcode/0090-Subsets-II/0090-Subsets-II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        # used = [False] * len(nums)
+
+        res = [[]]
+        seen = set()
+        def backTrack(first,path):
+            if path and tuple(path) not in seen:
+                seen.add(tuple(path))
+                res.append(path[:])
+
+            
+
+            for i in range(first,len(nums)):
+             
+                    path.append(nums[i])
+                    backTrack(i+1,path)
+                    path.pop()
+
+        backTrack(0,[])
+        return res
