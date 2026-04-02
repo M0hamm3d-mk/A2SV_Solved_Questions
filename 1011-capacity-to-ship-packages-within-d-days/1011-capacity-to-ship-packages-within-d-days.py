ae501a36cd087041aea10ece78f938f837f9123f
Mohammed Moges <mohammedmoges29@gmail.com>
2026-04-02 06:54:11 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -4,33 +4,23 @@ class Solution:
         def helper(max_cap):
             days_needed = 0
             running_sum = 0
-            limit = 0
+            # limit = 0
             for w in weights:
 
-                if running_sum + w < max_cap:
-                    running_sum += w
-                else:
-                    limit = max(limit,running_sum)
-                    running_sum = w
+                if running_sum + w > max_cap:
+                    running_sum = 0
                     days_needed += 1
-            if running_sum:
-                days_needed += 1
-            return [days_needed,limit]
+                running_sum += w
+           
+            return days_needed + 1
      
         low = max(weights)
         high = sum(weights)
-        if days == n:
-            return low
-        if days == 1:
-            return high
-        ans = high
         while low <= high:
             mid = (low + high) // 2
-            day,limit = helper(mid)
-            # print(low,mid,high,day)
+            day = helper(mid)
             if day <= days:
-                ans = min(ans,limit)   
                 high  = mid - 1
             else:
                 low = mid + 1
-        return ans
+        return low
