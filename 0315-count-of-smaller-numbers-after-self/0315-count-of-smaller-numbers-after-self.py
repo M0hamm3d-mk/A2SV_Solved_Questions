d35540221d3cd668937350b86a51d4d8ec1b5ea7
Mohammed Moges <mohammedmoges29@gmail.com>
2026-04-11 03:42:41 +0300

    Time: 927 ms (75.57%), Space: 63.9 MB (15.95%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,30 +1,45 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        def merge(left,right):
+        arr = nums
+        ans = [0] * len(arr)
+        def merge(left_ptr,right_ptr):
+            n,m = len(left_ptr),len(right_ptr)
             i = j = 0
-            n = len(left)
-            m = len(right)
-            merged = []
+            
+            merged_arr = []
+            # print(left_ptr,'----',right_ptr)
+            # cnt = 0
             while i < n and j < m:
-                if nums[left[i]] > nums[right[j]]:
-                    res[left[i]] += m-j
-                    merged.append(left[i])
-                    i += 1
-                else:
-                    merged.append(right[j])
+                if arr[left_ptr[i]] > arr[right_ptr[j]]:
+                    merged_arr.append(right_ptr[j])
                     j += 1
-            merged.extend(left[i:])
-            merged.extend(right[j:])
-            return merged
+                else:
+                    ans[left_ptr[i]] += j
+                    merged_arr.append(left_ptr[i])
+                    i += 1
+            while i < n:
+                ans[left_ptr[i]] += j
+                merged_arr.append(left_ptr[i])
+                i += 1
+            merged_arr.extend(right_ptr[j:])
+            
+            return merged_arr
 
-        def devide(arr):
+        def divide(arr):
             n = len(arr)
             if n == 1:
                 return arr
             mid = n // 2
-            return merge(devide(arr[:mid]),devide(arr[mid:]))
-        res = [0] * len(nums)
+            left = divide(arr[:mid])
+            right = divide(arr[mid:])
+            
+            return merge(left,right)
 
-        devide([i for i in range(len(nums))])
+        divide([x for x in range(len(arr))])
 
-        return res
+        return ans
+        
+    
+
+
+                    
