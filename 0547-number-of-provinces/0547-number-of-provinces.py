ad53b06ec3d53fc3eac3126a43834ed2e6a76355
Mohammed Moges <mohammedmoges29@gmail.com>
2026-04-23 13:44:32 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -3,17 +3,12 @@ class Solution:
         provinces = 0
         graph = defaultdict(list)
         n = len(isConnected)
-        for r in range(n):
-            for c in range(n):
-                if isConnected[r][c] and r != c:
-                    graph[r].append(c)
-                    graph[c].append(r)
         visited = [False] * n
-        def dfs(node):
-            visited[node]  = True
-            for neighbour in graph[node]: 
-                if not visited[neighbour]:
-                    dfs(neighbour)
+        def dfs(i):
+            visited[i]  = True
+            for j in range(n): 
+                if not visited[j] and isConnected[i][j]:
+                    dfs(j)
         for c in range(n):
             if not visited[c]:
                 dfs(c)
