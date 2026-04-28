50c6935e6ae43a04b8e9a65c7dad582c3e545262
Mohammed Moges <mohammedmoges29@gmail.com>
2026-04-28 13:05:30 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -3,27 +3,36 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
 
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        dn = [(0,1),(0,-1),(1,0),(-1,0)]
 
         def inbound(row,col):
             return 0 <= row < n and 0 <= col < m
         visited = [[False for _ in range(m)] for _ in range(n)]
-        def dfs(r,c,pr,pc):
-            visited[r][c] = True
-            for dr,dc in directions:
-                nr,nc = r + dr , c + dc
-                # if not inbound(nr,nc): continue
-                # elif grid[nr][nc] != grid[r][c]: continue
-                if inbound(nr,nc) and grid[nr][nc] == grid[r][c]:
-                    if not visited[nr][nc]:
-                        if dfs(nr,nc,r,c):
-                            return True
+        cycle = False
 
-                    elif (nr,nc) != (pr,pc):
-                        return True
+        def dfs(r,c,pr,pc):
+            # print("here")
+            nonlocal cycle
+            if cycle:
+                return 
+            visited[r][c]  = True
+            # print('here')
+            for dr,dc in dn:
+                nr = r + dr
+                nc = c + dc
+         
+                print(nr, nc)
+                if inbound(nr,nc) and  grid[nr][nc] == grid[r][c]:
+                    if not visited[nr][nc]:
+                        # visited[nr][nc] = True
+                        dfs(nr,nc,r,c)
+
+                    elif pr != nr or pc != nc:
+                        cycle = True
         for i in range(n):
             for j in range(m):
                 if not visited[i][j]:
-                    if dfs(i,j,-1,-1):
-                        return True
-        return False
+                    # print('here')
+                    dfs(i,j,-1,-1)
+
+        return cycle
