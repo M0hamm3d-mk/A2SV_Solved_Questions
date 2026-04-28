8dd99c30ba8a38b07beb2ed121d3bb3f6311ed14
Mohammed Moges <mohammedmoges29@gmail.com>
2026-04-28 14:06:20 +0300

    Time: 7 ms (28.29%), Space: 21.1 MB (31.34%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+
+        for a,b in prerequisites:
+            graph[b].append(a)
+
+        color = [0] * numCourses
+        # 0 not checked, 1 not taken -1 already taken
+        res = []
+        def dfs(course):
+
+            if color[course] == 1:
+                return False
+
+            elif color[course] == -1:
+                return True
+
+            color[course] = 1
+            for pre in graph[course]:
+                if not dfs(pre):
+                    return False
+
+            color[course] = -1
+
+            res.append(course)
+            return True
+
+        for i  in range(numCourses):
+            if not dfs(i):
+                return []
+                
+        return res[::-1]
+            
