7ee46368dcd1429949f60a51c6e2ced71686ae3e
Mohammed Moges <mohammedmoges29@gmail.com>
2026-05-12 11:16:27 +0300

    Time: 486 ms (9.39%), Space: 66 MB (28.91%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,25 +1,25 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         n = len(tasks)
-        heap = []
-        res = []
-        for i,t in enumerate(tasks):
-            t.append(i)
-        k = 0
-        t = 1
-        # print(tasks)
-        tasks.sort()
-        while len(res) < n:
-            while k < n and tasks[k][0] <= t:
-                    heappush(heap,(tasks[k][1],tasks[k][2]))
-                    k += 1
-            if heap:
-                curr = heappop(heap)
-                res.append(curr[1])
-                t += curr[0]
-            else:
-                t = tasks[k][0]
-        return res
+        for index,task in enumerate(tasks):
+            task.append(index)
         
+        tasks.sort()
+        print(tasks)
+        time = 1
+        order = []
+        i = 0
+        min_heap = []
+        while len(order) < n :
 
-            
+            while i < n and tasks[i][0] <= time:
+                heappush(min_heap,(tasks[i][1],tasks[i][2]))
+                i += 1
+
+            if min_heap:
+                completed = heappop(min_heap)
+                order.append(completed[1])
+                time += completed[0]
+            else:
+                time = tasks[i][0]
+        return order
