97f9f6b825d3ce2fe3d6752459a2c387779fe351
Mohammed Moges <mohammedmoges29@gmail.com>
2026-05-19 17:30:16 +0300

    Time: 0 ms (100%), Space: 19.3 MB (16.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = {0:0,1:1,2:2}
-        def ways(step,dp):
+        def ways(step):
             if step in dp:
                 return dp[step]
-            dp[step] = ways(step-1,dp) + ways(step-2,dp)
+            dp[step] = ways(step-1) + ways(step-2)
             return dp[step]
-        return ways(n,dp)
+        return ways(n)
             
